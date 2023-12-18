ffca218ac6431624b06fb72fa09c592b10883142
tk <139771088+thikyi@users.noreply.github.com>
2023-12-18 08:38:18 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data Science self-driven leaarning project 2',
+    author='Thant Thiri Myo Kyi',
+    license='MIT',
+)
